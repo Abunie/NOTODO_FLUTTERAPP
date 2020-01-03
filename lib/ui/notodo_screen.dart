adacb2c57b0f5c74998ad49e1840fac24c1d6e3b
Abunie <gaiya@ualberta.ca>
2020-01-03 11:39:38 -0700

    Final copy

--- lib/ui/notodo_screen.dart
+++ lib/ui/notodo_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:notodo_flutter_app/model/nodo_item.dart';
 import 'package:notodo_flutter_app/util/database_client.dart';
+import 'package:notodo_flutter_app/util/date_formatter.dart';
 class NotoDoScreen extends StatefulWidget{
   @override
   _NotoDoScreenState createState() => _NotoDoScreenState();
@@ -20,7 +21,7 @@ class _NotoDoScreenState extends State<NotoDoScreen> {
   void _handleSubmitted(String text) async{
     _textEditingController.clear();
 
-    NoDoItem noDoItem = new NoDoItem(text,DateTime.now().toIso8601String());
+    NoDoItem noDoItem = new NoDoItem(text,dateFormatted());
     int savedItemId = await db.saveItem(noDoItem);
     NoDoItem addedItem = await db.getItem(savedItemId);
     /**
@@ -46,7 +47,7 @@ class _NotoDoScreenState extends State<NotoDoScreen> {
                       color: Colors.white10,
                       child: new ListTile(
                         title: _itemList[index],
-                        onLongPress:() => debugPrint(""),
+                        onLongPress:() => _updateItem(_itemList[index], index),
                         trailing: new Listener(
                           key: new Key(_itemList[index].itemName),
                           child: new Icon(Icons.delete,
@@ -56,87 +57,9 @@ class _NotoDoScreenState extends State<NotoDoScreen> {
 
                 ),
                 );
-
-//           return Container(
-//             padding: new EdgeInsets.all(8.0),
-//             height: 50,
-//             child: Text('${_itemList[index].itemName}' , style: TextStyle(color: Colors.white, fontSize: 20),
-//             ),
-//           );
          },
-//         separatorBuilder: (BuildContext context, int index) => const Divider(),
        ),
 
-
-
-//        body:new Row(
-//          children: <Widget>[
-//            Expanded(
-//              child: SizedBox(
-//                height: 20.0,
-//                child: new ListView.builder(
-//                  scrollDirection: Axis.horizontal,
-//                  reverse: false,
-//                  itemCount: _itemList.length,
-//                  itemBuilder: (BuildContext ctxt, int index) {
-//                    return new Text(_itemList[index].itemName);
-////                    return new Card(
-////                      color: Colors.white10,
-////                      child: new ListTile(
-////                        title: _itemList[index],
-////    //                    onLongPress:() => debugPrint(""),
-////                        trailing: new Listener(
-////                          key: new Key(_itemList[index].itemName),
-////                          child: new Icon(Icons.delete,
-////                          color: Colors.redAccent,),
-////    //                      onPointerDown: (pointerEvent)=> debugPrint(""),
-////                        ),
-////
-////                ),
-////                );
-//                  },
-//                ),
-//              ),
-//            ),
-//            new IconButton(
-//              icon: Icon(Icons.remove_circle),
-//              onPressed: () {},
-//            ),
-//          ],
-//          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//        ),
-
-
-//      body: new Column(
-//        children: <Widget>[
-//          new Flexible(child: new ListView.builder(
-//              padding: new EdgeInsets.all(8.0),
-//              reverse: false,
-////              itemCount: _itemList.length,
-//              itemBuilder: (_,int index){
-//                return new Card(
-//                  color: Colors.white10,
-//                  child: new ListTile(
-//                    title: _itemList[index],
-////                    onLongPress:() => debugPrint(""),
-//                    trailing: new Listener(
-//                      key: new Key(_itemList[index].itemName),
-//                      child: new Icon(Icons.delete,
-//                      color: Colors.redAccent,),
-////                      onPointerDown: (pointerEvent)=> debugPrint(""),
-//                    ),
-//
-//                ),
-//                );
-//
-//              })),
-////          new Divider(
-////            height: 1.0,
-////          )
-//
-//
-//        ],
-//      ),
       floatingActionButton: new FloatingActionButton(
           tooltip: "Add Item",
           backgroundColor: Colors.pinkAccent,
@@ -208,6 +131,62 @@ class _NotoDoScreenState extends State<NotoDoScreen> {
     });
 
   }
+
+  _updateItem(NoDoItem item, int index){
+    var alert = new AlertDialog(
+      title: new Text("Edit Item"),
+      content: new Row(
+        children: <Widget>[
+          new Expanded(child: new TextField(
+            controller: _textEditingController,
+            autofocus: true,
+            decoration: new InputDecoration(
+              labelText: "Item",
+              hintText: "eg. Don't buy stuff",
+              icon: new Icon(Icons.edit)
+            ),
+
+          ))
+        ],
+      ),
+      actions: <Widget>[
+        new FlatButton(
+            onPressed: () async{
+              NoDoItem newItemUpdated = NoDoItem.fromMap(
+                {"itemName": _textEditingController.text,
+                 "dataCreated": dateFormatted(),
+                 "id": item.id }
+              );
+              _handleSubmittedUpdate(index, item); //redrawing the screen
+              await db.updateItem(newItemUpdated); //updating the item
+              setState(() {
+                _readNoDoList(); // redrawing the screen with all the items saved in the db
+              });
+              Navigator.pop(context);
+
+        },
+            child: new Text("Edit")),
+        new FlatButton(
+            onPressed: ()=> Navigator.pop(context),
+            child: new Text("Cancel"))
+
+      ]
+    );
+    showDialog(context:
+    context, builder:(_){
+      return alert;
+    });
+
+  }
+
+  void _handleSubmittedUpdate(int index, NoDoItem item){
+    setState(() {
+      _itemList.removeWhere((element){
+         return _itemList[index].itemName == item.itemName;
+      });
+    });
+  }
+
 
 
 }
---
+++ lib/util/date_formatter.dart
@@ -0,0 +1,7 @@
+import 'package:intl/intl.dart';
+String dateFormatted(){
+  var now = DateTime.now();
+  var formatter = new DateFormat("EEE, MMM d, yyyy");
+  String formatted = formatter.format(now);
+  return formatted;
+}
